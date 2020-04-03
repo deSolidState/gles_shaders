90eeead2ad1a8de8d65b91804aec792401ad45de
tim combs <desolidstate@gmail.com>
2020-04-03 11:06:13 -0700

    final fix to moire-maker.js to get history issue resolved - gotta love merge conflicts

--- moire_maker/moire-maker.js
+++ moire_maker/moire-maker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // include this file in a web page to make moire patterns using
 // the html5 canvas
 
@@ -26,33 +25,3 @@ function init() {
 }
 
 init();
-||||||| empty tree
-=======
-// include this file in a web page to make moire patterns using
-// the html5 canvas
-
-function draw() {
-  var c = document.getElementById('myCanvas');
-  var ctx = c.getContext('2d');
-  var time = new Date();
-
-  ctx.save();
-  ctx.clearRect(0, 0, 800, 600); // clear canvas
-
-  ctx.rotate(
-    ((2 * Math.PI) / 60) * time.getSeconds() +
-      ((2 * Math.PI) / 60000) * time.getMilliseconds()
-  );
-  ctx.fillRect(50, 20, 100, 50);
-
-  ctx.restore();
-
-  window.requestAnimationFrame(draw);
-}
-
-function init() {
-  window.requestAnimationFrame(draw);
-}
-
-init();
->>>>>>> 01cd2f68841de08e6ff7fdcd3adadcd4bfde79ff
