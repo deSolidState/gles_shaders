895f603a0537f9e44dc34680e817a1b148c2e69c
tim combs <desolidstate@gmail.com>
2020-04-03 12:28:48 -0700

    have moire-maker.js loading an image from repo and rotating two of them in opposite directions

--- moire_maker/moire-maker.js
+++ moire_maker/moire-maker.js
@@ -1,27 +1,46 @@
 // include this file in a web page to make moire patterns using
 // the html5 canvas
 
-function draw() {
-  var c = document.getElementById('myCanvas');
-  var ctx = c.getContext('2d');
-  var time = new Date();
+const img = new Image();
+img.src = './img/bookplate-001.png';
+img.onload = () => requestAnimationFrame(mainLoop); // start when loaded
 
-  ctx.save();
-  ctx.clearRect(0, 0, 800, 600); // clear canvas
+var c = document.getElementById('myCanvas');
+var ctx = c.getContext('2d');
+var startTime = new Date();
 
-  ctx.rotate(
+// background
+ctx.fillRect(20, 20, 150, 100);
+
+function drawImgRotated(img, x, y, scale, rot) {
+  ctx.setTransform(scale, 0, 0, scale, x, y);
+  ctx.rotate(rot);
+  ctx.drawImage(img, -img.width / 2, -img.height / 2);
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+  /* ctx.rotate(
     ((2 * Math.PI) / 60) * time.getSeconds() +
       ((2 * Math.PI) / 60000) * time.getMilliseconds()
   );
-  ctx.fillRect(50, 20, 100, 50);
+  ctx.fillRect(20, 50, 400, 600);
 
   ctx.restore();
-
-  window.requestAnimationFrame(draw);
+  */
 }
 
-function init() {
-  window.requestAnimationFrame(draw);
-}
+function mainLoop(time) {
+  ctx.clearRect(0, 0, 800, 600); // clear canvas
 
-init();
+  // background image with gradient fill
+  var gradient = ctx.createLinearGradient(0, 0, 170, 0);
+  gradient.addColorStop('0', 'magenta');
+  gradient.addColorStop('0.5', 'blue');
+  gradient.addColorStop('1.0', 'red');
+  ctx.fillStyle = gradient;
+  ctx.fillRect(20, 20, 150, 100);
+
+  drawImgRotated(img, 800 / 2, 300 / 2, 0.5, time / 1500);
+  drawImgRotated(img, 800 / 2, 300 / 2, 0.5, time / -1500);
+
+  requestAnimationFrame(mainLoop);
+}
