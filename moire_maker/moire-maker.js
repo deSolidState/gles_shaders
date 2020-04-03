3b76b98d8db2261804e8e40a00fb464891afecd0
tim combs <desolidstate@gmail.com>
2020-04-03 10:11:52 -0700

    create directory moire_maker & two files - simple index.html & moire-maker.js - to create moire patterns - current functionality rotates a rectangle on and off the canvas

---
+++ moire_maker/moire-maker.js
@@ -0,0 +1,27 @@
+// include this file in a web page to make moire patterns using
+// the html5 canvas
+
+function draw() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  var time = new Date();
+
+  ctx.save();
+  ctx.clearRect(0, 0, 300, 150); // clear canvas
+
+  ctx.rotate(
+    ((2 * Math.PI) / 60) * time.getSeconds() +
+      ((2 * Math.PI) / 60000) * time.getMilliseconds()
+  );
+  ctx.fillRect(50, 20, 100, 50);
+
+  ctx.restore();
+
+  window.requestAnimationFrame(draw);
+}
+
+function init() {
+  window.requestAnimationFrame(draw);
+}
+
+init();
